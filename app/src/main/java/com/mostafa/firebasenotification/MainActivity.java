13d50e521265fb5c8f9d6c7cf64e076fef7127d7
sirajul Mostafa <sirajulmost@gmail.com>
2021-09-16 14:17:48 +0600

    test key access

--- app/src/main/java/com/mostafa/firebasenotification/MainActivity.java
+++ app/src/main/java/com/mostafa/firebasenotification/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//just add this comment for test everything is work or not ?
         setContentView(R.layout.activity_main);
     }
 }
